c6a4e7fd696b43b71441b6a643657f33cabe28c0
be4lima <beatrizmlima@outlook.com.br>
2023-02-22 18:33:39 -0300

    Mapa perceptual

--- Correspondence analysis/Soil crop and fertilizer type.R
+++ Correspondence analysis/Soil crop and fertilizer type.R
@@ -69,3 +69,119 @@ qui2$residuals
 
 # Resíduos padronizados ajustados
 qui2$stdres
+
+# Mapa de calor dos resíduos padronizados ajustados
+data.frame(qui2$stdres) %>%
+  rename(Soil.Type = 1,
+         Crop.Type = 2) %>%
+  ggplot(aes(x = fct_rev(Soil.Type), y = Crop.Type,
+                         fill = Freq, label = round(Freq, 3))) +
+           geom_tile () +
+           geom_text(size = 5) +
+           scale_fill_gradient2(low = "white", 
+                                mid = "white", 
+                                high = "purple",
+                                midpoint = 1.96) +
+           labs( x = 'Tipo de solo', y = 'Tipo de cobertura', fill = "Res. Pad. Ajustados") +
+           coord_flip() +
+           theme_bw()
+
+## 2ª Parte: Análise da associação por meio do mapa perceptual
+
+# Definição da matriz A
+# Resíduos padronizados (qui2$residuals) divididos pela raiz quadrada do tamanho da amostra (n)
+matrizA <- qui2$residuals/sqrt(n)
+matrizA
+
+# Definição da matriz W
+# Multiplicação da matriz A transposta pela matriz A
+matrizW <- t(matrizA) %*% matrizA
+matrizW
+
+# Definição da quantidade de dimensões
+qtde_dimensoes <- min(nrow(matrizW) - 1, ncol(matrizW) - 1)
+qtde_dimensoes
+
+# Definição dos valores singulares
+VS_AV <- svd(matrizA, nu = qtde_dimensoes, nv = qtde_dimensoes)
+
+# Valores singulares de cada dimensão
+valores_singulares <- VS_AV$d[1:qtde_dimensoes]
+valores_singulares
+
+# Autovalores (eigenvalues) de cada dimensão
+eigenvalues <- (valores_singulares)^2
+eigenvalues
+
+# Cálculo da inércia principal total (a partir do qui-quadrado)
+inercia_total <- as.numeric(qui2$statistic/sum(tabela_contingencia))
+inercia_total
+
+# Cálculo da variância explicada em cada dimensão
+variancia_explicada <- eigenvalues / inercia_total
+variancia_explicada
+
+# Cálculo das massas das colunas (column profiles)
+soma_colunas <- apply(tabela_contingencia, MARGIN = 1, FUN = sum)
+soma_colunas
+
+# Massas das colunas (column profiles)
+massa_colunas <- soma_colunas / n
+massa_colunas
+
+# Cálculo das massas das linhas (row profiles)
+soma_linhas <- apply(tabela_contingencia, MARGIN = 2, FUN = sum)
+soma_linhas
+
+# Massas das linhas (row profiles)
+massa_linhas <- soma_linhas / n
+massa_linhas
+
+# Autovetores v das dimensões
+autovetor_v <-VS_AV$v
+autovetor_v
+
+# Autovetores u das dimensões
+autovetor_u <-VS_AV$u
+autovetor_u
+
+# Resumindo as informações até aqui
+data.frame(Dimensão = paste("Dimensão", 1:qtde_dimensoes),
+           `Valor Singular` = valores_singulares,
+           `Inércia Principal Parcial eigenvalues` = eigenvalues) %>%
+  mutate(`Percentual da Inércia Principal Total` = (`Inércia.Principal.Parcial.eigenvalues`/inercia_total) * 100,
+         `Percentual da Inércia Principal Total Acumulada` = cumsum(`Percentual da Inércia Principal Total`),
+         Qui2 = qui2$statistic[[1]] * `Percentual da Inércia Principal Total` / n,
+         `Valor Singular` = `Valor.Singular`,
+         `Inércia Principal Parcial eigenvalues` = Inércia.Principal.Parcial.eigenvalues) %>%
+  select(Dimensão, `Valor Singular`, `Inércia Principal Parcial eigenvalues`,
+         Qui2, `Percentual da Inércia Principal Total`,
+         `Percentual da Inércia Principal Total Acumulada`) %>%
+  kable() %>%
+  kable_styling(bootstrap_options = "striped", 
+                full_width = FALSE, 
+                font_size = 17)
+
+# Calculando as coordenadas para plotar as categorias no mapa perceptual
+
+# Variável em linha na tabela de contingência ('perfil')
+# Coordenadas das abcissas
+coord_abcissas_soil <- sqrt(valores_singulares[1]) * (massa_colunas^-0.5) * autovetor_u[,1]
+coord_abcissas_perfil
+
+# Coordenadas das ordenadas
+coord_ordenadas_soil <- sqrt(valores_singulares[2]) * (massa_colunas^-0.5) * autovetor_u[,2]
+coord_ordenadas_soil
+
+# Variável em coluna na tabela de contingência ('aplicacao')
+# Coordenadas das abcissas
+coord_abcissas_crop <- sqrt(valores_singulares[1]) * (massa_linhas^-0.5) * autovetor_v[,1]
+coord_abcissas_crop
+
+# Coordenadas das ordenadas
+coord_ordenadas_crop <- sqrt(valores_singulares[2]) * (massa_linhas^-0.5) * autovetor_v[,2]
+coord_ordenadas_crop
+
+# Mapa perceptual
+# O resultado pode ser obtido por meio da função 'CA' do pacote 'FactoMineR'
+anacor <- CA(tabela_contingencia, graph = TRUE)
